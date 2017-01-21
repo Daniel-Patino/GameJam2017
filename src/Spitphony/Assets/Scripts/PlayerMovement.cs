cd5ae1b154d9124b418d606fc0bf48c079d9eb9f
rslay <rhern283@fiu.edu>
2017-01-21 17:07:20 -0500

    Fixed player movement to use collisions rather than math

--- src/Spitphony/Assets/Scripts/PlayerMovement.cs
+++ src/Spitphony/Assets/Scripts/PlayerMovement.cs
@@ -19,18 +19,22 @@ public class PlayerMovement : MonoBehaviour {
         playerTrans = GetComponent<Transform>();
         rigidB = GetComponent<Rigidbody>();
         float oldV = playerTrans.position.y;
-    }
-	
+	}
+
+	void OnCollisionEnter(Collision other){
+		isInAir = false;
+	}
+
 	void FixedUpdate () {
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         oldV = playerTrans.position.y;
-        Debug.Log(oldV - newV);
+		Debug.Log((oldV - newV) + ", isInAir: "+ isInAir);
         Vector3 jumpForce = new Vector3(0.0f, jumpDist, 0.0f);
 
-        if (rigidB.velocity.y <= (0 + float.Epsilon) && rigidB.velocity.y >= (0 - float.Epsilon) && !isInAir)
+		if (!isInAir)
         {
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
@@ -45,7 +49,6 @@ public class PlayerMovement : MonoBehaviour {
         else
         {
             playerTrans.Translate(moveHorizontal * speed, 0.0f, 0.0f);
-            isInAir = false;
         }
 
         newV = playerTrans.position.y;
