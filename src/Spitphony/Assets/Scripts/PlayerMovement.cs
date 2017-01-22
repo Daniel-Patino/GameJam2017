0a403f19b009d0da7196934ca2d475374113c5dd
rslay <rhern283@fiu.edu>
2017-01-22 00:17:28 -0500

    Added healthbar functionality and sprite flipping

--- src/Spitphony/Assets/Scripts/PlayerMovement.cs
+++ src/Spitphony/Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
     private Transform playerTrans;
-    private Rigidbody rigidB;
+	private Rigidbody rigidB;
+	private SpriteRenderer spriteRender;
 
     private bool isInAir;
     private float oldV;
     private float newV;
+
+	public Image healthbar;
+	public float max_health = 100f;
+	public float cur_health = 0f;
 
     public float speed = 0.5f;
 	public float jumpDist = 450f;
@@ -17,14 +23,26 @@ public class PlayerMovement : MonoBehaviour {
 	public int maxSpeed = 8;
 
 	void Start () {
+		cur_health = max_health;
         isInAir = false;
+		spriteRender = GetComponent<SpriteRenderer>();
         playerTrans = GetComponent<Transform>();
         rigidB = GetComponent<Rigidbody>();
         float oldV = playerTrans.position.y;
+		InvokeRepeating ("decreaseHealth", 0f, 2f);
 	}
 
 	void OnCollisionEnter(Collision other){
 		isInAir = false;
+	}
+
+	void decreaseHealth(){
+		cur_health -= 5f;
+		SetHealth (cur_health / max_health);
+	}
+
+	void SetHealth(float health) {
+		healthbar.fillAmount = health;
 	}
 
 	void FixedUpdate () {
@@ -45,6 +63,7 @@ public class PlayerMovement : MonoBehaviour {
 		} 
 		if (moveHorizontal < 0) {
 
+			spriteRender.flipX = true;
 			if (isInAir) {
 				Vector3 walkForce = new Vector3 (-1*walkDist, 0.0f, 0.0f);
 				rigidB.AddForce (walkForce);
@@ -54,6 +73,7 @@ public class PlayerMovement : MonoBehaviour {
 			}
 		} else if (moveHorizontal > 0) {
 
+			spriteRender.flipX = false;
 			if (isInAir) {
 				Vector3 walkForce = new Vector3 (1*walkDist, 0.0f, 0.0f);
 				rigidB.AddForce (walkForce);
