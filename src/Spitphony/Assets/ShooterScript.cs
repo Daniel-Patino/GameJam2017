9e6cab4008b43b567a04fc77e6df3d6fc95e3985
DESKTOP-4CDN43V\Daniel <dpati007@fiu.edu>
2017-01-22 09:45:25 -0500

    Not Much

---
+++ src/Spitphony/Assets/ShooterScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour {
+
+    public GameObject lazerBeam;
+    public Transform showSpawn;
+
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject shots = Instantiate(lazerBeam, showSpawn.position, showSpawn.rotation) as GameObject;
+        }
+    }
+}
