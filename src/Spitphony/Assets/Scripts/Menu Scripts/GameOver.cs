e19f0a2d45af57c579510b0c9fec6c83a829181d
Ernesto Rojo Jr <gamedev@ernestorojo.com>
2017-01-22 14:59:52 -0500

    Added buttons

---
+++ src/Spitphony/Assets/Scripts/Menu Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        LevelManager.Instance.LoadScene(LevelManager.Stage1SceneName);
+        LevelManager.Instance.LoadSceneAdditive(LevelManager.MusicalEnvSceneName);
+    }
+}
