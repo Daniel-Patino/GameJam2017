e75e10f2a21b83299ba5ea0492506409c2b6eebf
rslay <rhern283@fiu.edu>
2017-01-21 20:44:59 -0500

    Collisions cleaned up and max speed set

--- src/Spitphony/Assets/Scripts/PlayerMovement.cs
+++ src/Spitphony/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
 
     private Transform playerTrans;
     private Rigidbody rigidB;
@@ -12,49 +11,81 @@ public class PlayerMovement : MonoBehaviour
     private float oldV;
     private float newV;
 
-    public float speed;
-    public float jumpDist;
+    public float speed = 0.5f;
+	public float jumpDist = 450f;
+	public float walkDist = 25f;
+	public int maxSpeed = 8;
 
-    void Start()
-    {
+	void Start () {
         isInAir = false;
         playerTrans = GetComponent<Transform>();
         rigidB = GetComponent<Rigidbody>();
         float oldV = playerTrans.position.y;
-    }
+	}
 
-    void OnCollisionEnter(Collision other)
-    {
-        isInAir = false;
-    }
+	void OnCollisionEnter(Collision other){
+		isInAir = false;
+	}
 
-    void FixedUpdate()
-    {
+	void FixedUpdate () {
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        oldV = playerTrans.position.y;
-        Debug.Log((oldV - newV) + ", isInAir: " + isInAir);
-        Vector3 jumpForce = new Vector3(0.0f, jumpDist, 0.0f);
+		oldV = playerTrans.position.y;
+		Debug.Log((oldV - newV) + ", isInAir: "+ isInAir);
+		Vector3 jumpForce = new Vector3(0.0f, jumpDist, 0.0f);
 
-        if (!isInAir)
-        {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                rigidB.AddForce(jumpForce);
-                isInAir = true;
-            }
-            else
-            {
-                playerTrans.Translate(moveHorizontal * speed, 0.0f, 0.0f);
-            }
-        }
-        else
-        {
-            playerTrans.Translate(moveHorizontal * speed, 0.0f, 0.0f);
-        }
 
-        newV = playerTrans.position.y;
+//		if (rigidB.velocity.y <= 0 + float.Epsilon && !isInAir) // replaced with collision testing
+		if (!isInAir)
+        {
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				rigidB.AddForce (jumpForce);
+				isInAir = true;
+			} 
+		} 
+		if (moveHorizontal < 0) {
+
+			if (isInAir) {
+				Vector3 walkForce = new Vector3 (-1*walkDist, 0.0f, 0.0f);
+				rigidB.AddForce (walkForce);
+			} else {
+				Vector3 walkForce = new Vector3 (-2*walkDist, 0.0f, 0.0f);
+				rigidB.AddForce (walkForce);
+			}
+		} else if (moveHorizontal > 0) {
+
+			if (isInAir) {
+				Vector3 walkForce = new Vector3 (1*walkDist, 0.0f, 0.0f);
+				rigidB.AddForce (walkForce);
+			} else {
+				Vector3 walkForce = new Vector3 (2*walkDist, 0.0f, 0.0f);
+				rigidB.AddForce (walkForce);
+			}
+
+		}
+
+//		if (rigidB.velocity.y > 20) {
+//			rigidB.velocity.y = 20;
+//		}else if(rigidB.velocity.y < -20){
+//			rigidB.velocity.y = -20;
+//		}
+
+//		if (rigidB.velocity.x > maxHozSpeed) {
+//			rigidB.velocity = new Vector3(rigidB.velocity.x, maxHozSpeed, rigidB.velocity.z);
+//		}else if(rigidB.velocity.x < -1*maxHozSpeed){
+//			rigidB.velocity = new Vector3(rigidB.velocity.x, -1*maxHozSpeed, rigidB.velocity.z);
+//		}
+		if(rigidB.velocity.magnitude > maxSpeed){
+			rigidB.velocity = Vector3.ClampMagnitude(rigidB.velocity, maxSpeed);
+		}
+
+		Debug.Log ("Y: "+rigidB.velocity.y);
+
+
+		Debug.Log ("X: "+rigidB.velocity.x);
+        
+		newV = playerTrans.position.y;
     }
 }
