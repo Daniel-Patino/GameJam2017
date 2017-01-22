270788686a454520837b3edb75c7e5cff8962b0e
DESKTOP-4CDN43V\Daniel <dpati007@fiu.edu>
2017-01-21 21:00:06 -0500

    Scripts to Move Items Up and Down

---
+++ src/Spitphony/Assets/Scripts/UpAndDown.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpAndDown : MonoBehaviour {
+
+    private Transform trans;
+    private float startingVert;
+    private bool movingUp;
+
+    public float intensity;
+    public float distance;
+
+	// Use this for initialization
+	void Start () {
+        trans = GetComponent<Transform>();
+        startingVert = trans.position.y;
+        movingUp = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        if(trans.position.y < startingVert + distance && movingUp)
+        {
+            trans.Translate(0.0f, +intensity, 0.0f);
+            Debug.Log(trans.position.y);
+            if (trans.position.y >= startingVert + distance)
+            {
+                movingUp = false;
+            }
+        }
+        
+        if(trans.position.y > startingVert && !movingUp)
+        {
+            trans.Translate(0.0f, -intensity, 0.0f);
+            if (trans.position.y <= startingVert)
+            {
+                movingUp = true;
+            }
+        }
+	}
+}
