4793bddcf8c4e7c7fb461d54d68ada3e61dc8a9d
rslay <rhern283@fiu.edu>
2017-01-22 10:54:19 -0500

    Broken attacking, trying to fix speed

---
+++ src/Spitphony/Assets/Scripts/LlamaAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LLamaAttack : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	public GameObject llama;
+	private SpriteRenderer spriteRender;
+	private int alt;
+
+	void Start () {
+
+		spriteRender = llama.GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody>();
+		if (spriteRender.flipX) {
+			alt = 1;
+		} else {
+			alt = -1;
+		}
+
+		rb.velocity = transform.right * alt * speed;
+	} 
+}
